5927cc062788fc087cfd45d691a01e2ee0011419
nuttasart <nuttasartac126@gmail.com>
2020-12-21 14:34:20 +0700

    App

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -15,7 +16,38 @@ class _AuthenState extends State<Authen> {
           children: [
             buildLogo(),
             buildAppName(),
+            buildUser(),
+            buildPassword(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Password',
+        ),
+        autofocus: false,
+        obscureText: true,
+      ),
+    );
+  }
+
+  Container buildUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.perm_identity),
+          border: OutlineInputBorder(),
+          hintText: 'Username',
         ),
       ),
     );
@@ -23,18 +55,19 @@ class _AuthenState extends State<Authen> {
 
   Text buildAppName() => Text(
         'NUT AIC App',
-        style: TextStyle(
+        style: GoogleFonts.zcoolKuaiLe(
+            textStyle: TextStyle(
           fontSize: 27,
           fontWeight: FontWeight.bold,
           color: Colors.indigo.shade900,
           fontStyle: FontStyle.italic,
-        ),
+        )),
       );
 
   Container buildLogo() {
     return Container(
       width: 150,
-      child: Image.asset('images/logo.png'),
+      child: Image.asset('images/AIC.png'),
     );
   }
 }
